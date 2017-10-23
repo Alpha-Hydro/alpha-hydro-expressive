53dda85b7a2870053a65b46250e1609612af999c
VladMeh <vladmeh@gmail.com>
2017-10-23 17:57:41 +0300

    sidebar dropdown

--- source/index.js
+++ source/index.js
@@ -1,2 +1,22 @@
 import './javascript/bootstrap/collapse';
 import './javascript/bootstrap/transition';
+
+let toggle = $('.sidebar-toggle');
+let dropdown = $('.sidebar-dropdown');
+
+toggle.click(function (e) {
+	e.preventDefault();
+	e.stopPropagation();
+
+	$('html').css('overflow', 'hidden');
+	dropdown.toggleClass('open');
+
+	$(document).one('click', function closeMenu(e) {
+		if (dropdown.has(e.target).length === 0){
+			dropdown.removeClass('open');
+			$('html').css('overflow', 'auto');
+		}else{
+			$(document).one('click', closeMenu);
+		}
+	})
+});
