1db024bd4b0a7fb47cd233db10e996d71489c3f9
VladMeh <vladmeh@gmail.com>
2017-12-05 17:00:27 +0300

    public deploy

--- data/categories.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE categories CHANGE parent_id parent_id BIGINT NOT NULL, CHANGE active active TINYINT(1) NOT NULL, CHANGE sorting sorting
-  INT NOT NULL, CHANGE deleted deleted INT NOT NULL;
-
-ALTER TABLE categories ENGINE=InnoDB;
--- data/subproduct_params_values.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE subproduct_params_values ADD id INT NOT NULL PRIMARY KEY AUTO_INCREMENT;
-CREATE UNIQUE INDEX subproduct_params_values_id_uindex ON subproduct_params_values (id);
-
-ALTER TABLE subproduct_params_values ENGINE=InnoDB;
