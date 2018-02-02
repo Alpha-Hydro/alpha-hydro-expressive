73b2322652fb41799bb683707b66a00967d5c18a
VladMeh <vladmeh@gmail.com>
2018-02-03 02:04:52 +0300

    search query

--- src/Search/src/Action/SearchPageAction.php
+++ src/Search/src/Action/SearchPageAction.php
@@ -47,20 +47,14 @@ class SearchPageAction implements ServerMiddlewareInterface
     {
 
         $queryParams = $request->getQueryParams();
-        $query = str_replace(array('.',',',' ','-','_','/','\\','*','+','&','^','%','#','@','!','(',')','~','<','>',':',';','"',"'","|"), '', $queryParams['query']);
-        $sQuery = "MATCH(s_name, sku, name, meta_keywords) AGAINST ('+$query*' IN BOOLEAN MODE)";
-        $sql = "SELECT * FROM products p WHERE MATCH (s_name, sku, name, meta_keywords) AGAINST ('+any*' IN BOOLEAN MODE)";
 
-        $rsm = new ResultSetMapping();
+        $resultSearch = $this->entityManager->getRepository(Products::class)
+            ->searchSqlQuery($queryParams['query']);
 
-        $rsm->addEntityResult(Products::class, 'p');
-        $rsm->addFieldResult('p', 'name', 'name');
-        $rsm->addFieldResult('p', 'sku', 'sku');
+        $data = [];
+        foreach ($resultSearch as $item)
+            $data[] = $item->getName();
 
-        $nativeQuery = $this->entityManager->createNativeQuery($sql, $rsm);
-
-        $result = $nativeQuery->getResult();
-
-        return new JsonResponse($result);
+        return new JsonResponse($data);
     }
 }
